d08253b45beb96f8c10a867eefa14d5435c3db6b
george <geomel@yandex.ru>
2024-12-04 22:47:59 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     id("java")
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -16,4 +20,13 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+tasks.jacocoTestReport { reports { xml.required.set(true) }
 }
