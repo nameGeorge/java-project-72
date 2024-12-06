e0564d40c5175da1e2e3bcf13fffdd591804640a
george <geomel@yandex.ru>
2024-12-06 18:59:26 +0300

    app.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,18 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     id("java")
-    checkstyle
-    jacoco
     application
-    id("checkstyle")
-    id("io.freefair.lombok") version "8.4"
-    id("com.github.ben-manes.versions") version "0.51.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    checkstyle
 }
-
+application { mainClass.set("hexlet.code.App") }
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-application {
-    mainClass.set("hexlet.code.App")
-}
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.10")//logger
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.0.1")
-    implementation("gg.jte:jte:3.0.1")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
 tasks.test {
     useJUnitPlatform()
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-
-        showStandardStreams = true
-    }
-}
-tasks.jacocoTestReport { reports { xml.required.set(true) }
 }
