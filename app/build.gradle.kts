3797e7aea0c525b49b101ce37964d37a8eb73691
george <geomel@yandex.ru>
2024-12-06 21:54:17 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,40 +1,25 @@
 plugins {
     id("java")
-    application
     checkstyle
     jacoco
-    id("io.freefair.lombok") version "8.6"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-
 }
-
-application { mainClass.set("hexlet.code.App") }
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.13")
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("com.h2database:h2:2.2.224")
-    implementation("org.postgresql:postgresql:42.7.3")
-    implementation("gg.jte:jte:3.1.12")
 }
-
 tasks.test {
     useJUnitPlatform()
-
-finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
         xml.required = true
     }
