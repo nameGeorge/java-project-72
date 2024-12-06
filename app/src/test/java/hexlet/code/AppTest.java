8e87d396eb484418c168ff07f246f40629371e49
george <geomel@yandex.ru>
2024-12-06 23:45:23 +0300

    step3.12

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
@@ -16,22 +14,22 @@ public class AppTest {
     private static String baseUrl;
 
     @BeforeAll
-    public static void beforeAll() throws SQLException, IOException {
-        app = App.getApp();
-        app.start(0);
-        int port = app.port();
-        baseUrl = "http://localhost:" + port;
-    }
+        public static void beforeAll() throws SQLException, IOException {
+            app = App.getApp();
+            app.start(0);
+            int port = app.port();
+            baseUrl = "http://localhost:" + port;
+        }
 
-    @AfterAll
-    public static void afterAll() {
-        app.stop();
-    }
+        @AfterAll
+        public static void afterAll() {
+            app.stop();
+        }
 
-    @Test
-    void testRootPage() throws Exception {
-        //HttpResponse<String> response = (HttpResponse<String>) Unirest.get(baseUrl + "/").asString();
-        //assertTrue("200".equals(((kong.unirest.HttpResponse<?>) response).getStatus()));
-        assertTrue(true);
+        @Test
+        void testRootPage() throws Exception {
+            //HttpResponse<String> response = (HttpResponse<String>) Unirest.get(baseUrl + "/").asString();
+            //assertTrue("200".equals(((kong.unirest.HttpResponse<?>) response).getStatus()));
+            assertTrue(true);
+        }
     }
-}
