914b104fed58af40dca632545782c05ac0d11e08
george <geomel@yandex.ru>
2024-12-08 21:23:13 +0300

    step8.1

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code.dto;
-import lombok.Getter;
-import lombok.Setter;
-@Getter
-@Setter
-public class BasePage {
-    private String flash;
-    private String flashType;
-}
--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package hexlet.code.repository;
-
-import com.zaxxer.hikari.HikariDataSource;
-
-public class BaseRepository {
-    public static HikariDataSource dataSource;
-}
-
